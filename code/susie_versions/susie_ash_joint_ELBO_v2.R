0e6f0e85ed345ed48f05fd0f2aa707b4436ffdd2
alexmccreight <57416850+alexmccreight@users.noreply.github.com>
2024-10-14 10:51:20 -0400

    fixed convergence criteria

--- code/susie_versions/susie_ash_joint_ELBO_v2.R
+++ code/susie_versions/susie_ash_joint_ELBO_v2.R
@@ -404,8 +404,9 @@ susie_ash = function (X,y,L = min(10,ncol(X)),
       # Step 5: Convergence Criterion
       # elbo[i+1] =  -(mrash_output$varobj)[length(mrash_output$varobj)] - term3/(2*mrash_output$sigma2) + sum(s$KL) #Issue which sigma^2 they used?
       if(est_var == "cal_v"){
-        elbo[i+1] =  -(mrash_output$varobj)[length(mrash_output$varobj)] - term3/(2*mrash_output$sigma2) + term5  # Joint ELBO
-      }else if(est_var != "cal_v"){
+        #elbo[i+1] =  -(mrash_output$varobj)[length(mrash_output$varobj)] - term3/(2*mrash_output$sigma2) + term5  # Joint ELBO
+        elbo[i+1] = max(abs(s$prev_alpha - s$alpha))
+        }else if(est_var != "cal_v"){
         elbo[i+1] = max(abs(s$prev_alpha - s$alpha)) # SuSiE-Inf convergence criterion
       }
       #
@@ -445,9 +446,18 @@ susie_ash = function (X,y,L = min(10,ncol(X)),
       }
 
       #Convergence Criterion
-      if (elbo[i+1] - elbo[i] < tol) {
+      if(est_var == "cal_v"){
+      if (abs(elbo[i+1] - elbo[i]) < tol) {
         s$converged = TRUE
         break
+      }
+      }
+
+      if(est_var == "mom"){
+      if (elbo[i+1] < tol) {
+        s$converged = TRUE
+        break
+      }
       }
     }
 
